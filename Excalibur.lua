5c3e8ead0d8f66d8a2faa28ce15c8cf205091f39
Glxcier97 <102719178+Glxcier97@users.noreply.github.com>
2022-07-06 22:00:01 -0400

    Update Excalibur.lua

--- Excalibur.lua
+++ Excalibur.lua
@@ -1,4 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Tescalus/Pendulum-Hubs-Source/main/Reanimation.lua"))()
 warn("Netless Activated!")
 _G.loop = true
 local player = game.Players.LocalPlayer
